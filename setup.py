28be2ac8101a6bb712c7466a23123d0ddea0dd6b
burdinskiy-quantnet <burdinskiy@quantnet.ai>
2025-05-16 14:41:35 +0700

    update package dependencies for compatibility

--- setup.py
+++ setup.py
@@ -2,22 +2,22 @@ from setuptools import setup
 
 setup(
     name="qnt",
-    version="0.0.501",
+    version="0.0.502",
     url="https://quantiacs.com",
     license='MIT',
     packages=['qnt', 'qnt.ta', 'qnt.data', 'qnt.examples'],
     package_data={'qnt': ['*.ipynb']},
     install_requires=[
-        'scipy>=1.14.0',
-        'pandas==2.2.3',
-        'xarray==2025.3.1',
-        'numpy<2.3.0',
-        'tabulate>=0.9.0',
-        'bottleneck>=1.4.2',
-        'numba==0.61.2',
+        'scipy>=1.14.0,<2.0.0',
+        'pandas>=2.2,<2.3',
+        'xarray>=2024.11.0,<2026.0.0',
+        'numpy>=1.26.0,<2.3.0',
+        'tabulate>=0.9.0,<1.0.0',
+        'bottleneck>=1.4.2,<2.0.0',
+        'numba>=0.59.0,<1.0.0',
         'progressbar2>=3.55,<4',
-        'cftime==1.6.4',
-        'plotly==6.0.1',
-        'matplotlib==3.10.1'
+        'cftime>=1.6.4,<2.0.0',
+        'plotly>=5.22,<6.1',
+        'matplotlib>=3.10.1,<4.0.0'
     ]
 )
