9f50f71ff1859e079fc99a0a8d5ae5a5b682317c
burdinskiy-quantnet <burdinskiy@quantnet.ai>
2025-08-01 15:17:48 +0700

    Update exits.py with changes to max_hold_long and max_hold_short functions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="qnt",
-    version="0.0.504",
+    version="0.0.505",
     url="https://quantiacs.com",
     license='MIT',
     packages=['qnt', 'qnt.ta', 'qnt.data', 'qnt.examples'],
